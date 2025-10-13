92da6fc2387ee2fa09e5233ea95afb2e24ffec72
jsturges640-Shelfwhiz <jsturges640@gmail.com>
2025-10-13 13:03:26 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -90,7 +90,7 @@ export default function HomePage() {
             </div>
             <div className="animate-fade-in-left">
               <Image
-                src="https://imgur.com/a/WmVeXo8"
+                src="https://imgur.com/a/WmVeXo8.png"
                 alt="ShelfWhiz Logo"
                 width={500}
                 height={500}
