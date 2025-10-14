cea3ed7f18bf055508d5f58cf5cd9bf8d71deccb
jsturges640-Shelfwhiz <jsturges640@gmail.com>
2025-10-14 10:29:55 -0700

    Update page.tsx

--- app/testimonials/page.tsx
+++ app/testimonials/page.tsx
@@ -87,8 +87,8 @@ const testimonials = [
   },
   {
     id: 8,
-    name: "Scott Austin",
-    company: "NAW",
+    name: "Scott A",
+    company: "St. Helens, OR",
     role: "Director of Sales",
     content: "As a factory rep, I’ve worked with countless distribution centers, and the shelf mapping service has consistently made new product placement faster, easier, and more efficient. John delivers clear, well-structured layouts that eliminate confusion and ensure parts are slotted exactly where they need to be. This not only speeds up the process of putting parts on the shelf but also helps maintain flow-through accuracy and minimizes disruption to existing inventory. It’s a huge win for both our customers and our teams.",
     rating: 5,
